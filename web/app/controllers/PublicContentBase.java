47b7a4c8deacb06987c451870550c5772bcf909d
Javi Espinosa <javier.espinosa@11paths.com>
2021-05-12 20:41:07 +0200

    fix: Add Password policy

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -12,6 +12,12 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
+        if (password != null && password.length() < 8){
+            flash.error("Password too short");
+            register();
+            return;
+        }
+
         User u = new User(username, HashUtils.getMd5(password), type, -1);
         u.save();
         registerComplete();
